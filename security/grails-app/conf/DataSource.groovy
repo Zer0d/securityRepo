921c37bd50d8c0e1e25e6d81d14f3cff8a4f9b62
Adrian Kelly <akelly@dius.com.au>
2012-09-30 15:32:04 +1000

    Test datasource

--- security/grails-app/conf/DataSource.groovy
+++ security/grails-app/conf/DataSource.groovy
@@ -6,22 +6,48 @@ hibernate {
 		region.factory_class='net.sf.ehcache.hibernate.EhCacheRegionFactory'
 	}
 }
-dataSource {
-	pooled=true
-	dbCreate='update'
-	url='jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8'
-	driverClassName='com.mysql.jdbc.Driver'
-	dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
-	username='zerodusername'
-	password='zerodpassword'
-	properties {
-		maxActive=50
-		maxIdle=25
-		minIdle=5
-		initialSize=5
-		minEvictableIdleTimeMillis=60000
-		timeBetweenEvictionRunsMillis=60000
-		maxWait=10000
-		validationQuery='SELECT 1'
+environments {
+	test{
+		dataSource {
+		pooled=true
+		dbCreate='update'
+		url='jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8'
+		driverClassName='com.mysql.jdbc.Driver'
+		dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
+		username='zerodusername'
+		password='zerodpassword'
+			properties {
+				maxActive=5
+				maxIdle=2
+				minIdle=1
+				initialSize=5
+				minEvictableIdleTimeMillis=60000
+				timeBetweenEvictionRunsMillis=60000
+				maxWait=10000
+				validationQuery='SELECT 1'
+			}
+		}
+	}
+    production{
+	dataSource {
+		pooled=true
+		dbCreate='update'
+		url='jdbc:mysql://localhost/zeroddbname?useUnicode=yes&characterEncoding=UTF-8'
+		driverClassName='com.mysql.jdbc.Driver'
+		dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
+		username='zerodusername'
+		password='zerodpassword'
+		properties {
+			maxActive=50
+			maxIdle=25
+			minIdle=5
+			initialSize=5
+			minEvictableIdleTimeMillis=60000
+			timeBetweenEvictionRunsMillis=60000
+			maxWait=10000
+			validationQuery='SELECT 1'
+		}
 	}
 }
+}
+
